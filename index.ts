30ceeb30c115ed9ebfa75b919ea92644a0dad7f4
Alexander Zhibul <zhibul.alexander@gmail.com>
2023-07-09 00:49:40 +0400

    feat: update index.ts and env, add new types

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config';
 
 import { httpServer } from './src/http_server';
+import { CreateWebSocketServer } from './src/websocket_server';
 
-const HTTP_PORT = process.env.HTTP_PORT || 8181;
+const HTTP_PORT: number = Number(process.env.HTTP_PORT) || 8181;
+const WEB_SOCKET_PORT: number = Number(process.env.WEB_SOCKET_PORT) || 4000;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+new CreateWebSocketServer(WEB_SOCKET_PORT);
+
+console.log(`Start static http server on the ${HTTP_PORT} port and websocket server on the ${WEB_SOCKET_PORT} port, good luck!`);
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,32 @@
-export {};
+import { TYPES } from '../constants';
+
+export interface RegistrationRequest {
+  type: typeof TYPES.REG;
+  data: {
+    name: string;
+    password: string;
+  };
+  id: number;
+}
+
+export interface RegistrationResponse {
+  type: typeof TYPES.REG;
+  data: {
+    name: string;
+    index: number;
+    error: boolean;
+    errorText: string;
+  };
+  id: number;
+}
+
+export interface UpdateWinners {
+  type: typeof TYPES.UPDATE_WINNERS,
+  data: [
+    {
+      name: string;
+      password: string;
+    },
+  ];
+  id: number;
+}
